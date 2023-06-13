6ef104f5cfc9aeebdba3d25e496ba109ef537a69
Divya Karki <karkidivya5@.com>
2023-06-13 23:08:16 +0545

    test1

--- client/e-commerce-website/src/App.js
+++ client/e-commerce-website/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { Home } from './pages/Home/Home';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Cart } from './pages/cart/cart';
+import { productsData } from './api/Api';
+import { createBrowserRouter,Outlet, RouterProvider, ScrollRestoration } from 'react-router-dom';
+import { Product } from './pages/product/product';
+import { Login } from './pages/login/Login';
+
+
+const Layout = ()=>{
+  return <div>
+    <Header/>
+    <ScrollRestoration/>
+    <Outlet/>
+    <Footer/>
+  </div>
+}
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Layout/>,
+    children:[
+      {
+        path: '/',
+        element: <Home/>,
+        loader: productsData
+      },
+      {
+        path:'/product/:id',
+        element: <Product/>
+      },
+      {
+        path: '/cart',
+        element: <Cart/>
+      },
+      {
+        path: '/login',
+        element: <Login/>
+      }
+    ]
+  }
+]) 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <RouterProvider router={router}/>
     </div>
   );
 }
